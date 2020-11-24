b69ada9a26cefb272a9cd0e84228debcd83e2855
WinnterWolf <patrick.winter@outlook.com.br>
2020-11-24 18:02:28 -0400

    implementando Ajax requests

--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import * as $ from 'jquery';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+
 
   private baseUrl = 'http://localhost:8080/api/v1/usuarios';
 
@@ -17,6 +21,22 @@ export class UsuarioService {
 
   createUsuario(usuario: Object): Observable<Object> {
     return this.http.post(`${this.baseUrl}`, usuario);
+  //   return $.ajax({
+  //     url: `${this.baseUrl}`,
+  //     type: "POST",
+  //     data: JSON.stringify({usuario}),
+  //     dataType: "json",
+  //     contentType: "application/json; charset=utf-8",
+  
+  // }).done(function(resposta) {
+  //     console.log(resposta);
+  
+  // }).fail(function(jqXHR, textStatus ) {
+  //     console.log("Request failed: " + textStatus);
+  
+  // }).always(function() {
+  //     console.log("completou");
+  // });
   }
 
   updateUsuario(id: number, value: any): Observable<Object> {
